c054edcb59b495a1f8d83cd5b4018e8da6e70e92
pdxbellasaurus <carrieannyoung@gmail.com>
2021-05-22 00:27:23 -0700

    minor fix

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-
-const Workout = require('../models/workout.js');
+const Workout = require('../models');
 
 router.get('/api/workouts', (req, res) => {
   Workout.find({})
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require('morgan')
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,9 +19,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   }
 );
 
-app.use(require('routes/api.js'));
-app.use(require('routes/html.js'));
+app.use(require('./routes/api.js'));
+app.use(require('./routes/html.js'));
 
 app.listen(PORT, () => {
-  // console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
